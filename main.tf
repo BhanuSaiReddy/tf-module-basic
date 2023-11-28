b80acb0b88fb9707b0373a2105f92cd169539eb1
bhanu <bhanusaireddy5244@gmail.com>
2023-11-28 20:25:43 +0530

    terraform.

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "instance" {
 
 resource "aws_route53_record" "record" {
   zone_id = var.zone_id
-  name    = "${var.name}-devopsbs344.online"
+  name    = "${var.name}-dev.devopsbs344.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance.private_ip]
